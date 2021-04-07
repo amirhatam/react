c5669a35548b07fe3b5ae903c0291ec6d51e189b
Amir <amirhatam136@gmail.com>
2021-04-07 15:15:10 +0200

    ex1

---
+++ chapitre-01/elsy/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import './App.css';
+
+var tempMin = -20; 
+var tempMax = 40;
+var heartMin = 80;
+var heartMax = 180;
+var stepsMin = 0;
+var stepsMax = 50000; 
+
+class App extends React.Component {
+  render() {
+    return (
+      <div class= "container-fluid">
+        <div class = "row">
+          
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
