9623c03d71bd7af3eebbe8c0349802ecaf7412bd
Amir <amirhatam136@gmail.com>
2021-04-08 21:20:10 +0200

    refaire

---
+++ refaire-elsy/src/components/Box.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+
+
+class Box extends React.Component {
+
+    render() {
+
+        return (
+
+            <div className="box col-sm-3 col-6" style={{ fontSize: "100px" }}>
+                <span class="material-icons">directions_walk</span>
+                <p></p>
+            </div>
+
+
+        )
+
+
+
+    }
+
+
+
+}
+
+export default Box;
