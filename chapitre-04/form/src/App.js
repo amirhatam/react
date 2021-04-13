8d76d48eabce8e04ddf527ff1dd3e02a56692cd0
Amir <amirhatam136@gmail.com>
2021-04-13 17:25:49 +0200

    fini formulaire

--- chapitre-04/form/src/App.js
+++ chapitre-04/form/src/App.js
@@ -1,11 +1,39 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      activeTab: "add",
+      item: [],
+    };
+  }
+
   render() {
     return (
       <div>
-      
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+           
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Remember me" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
       </div>
     );
   }
