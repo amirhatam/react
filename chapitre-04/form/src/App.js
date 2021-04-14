af43f7873cff99ff063a0b100997a7bb65f725a5
Amir <amirhatam136@gmail.com>
2021-04-14 14:11:45 +0200

    blc

--- chapitre-04/form/src/App.js
+++ chapitre-04/form/src/App.js
@@ -3,37 +3,43 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
-class App extends React.Component {
-  constructor() {
-    super();
 
-    this.state = {
-      activeTab: "add",
-      item: [],
-    };
-  }
+
+
+
+
+
+
+
+
+class App extends React.Component {
+  
 
   render() {
     return (
-      <div>
-        <Form>
-          <Form.Group controlId="formBasicEmail">
+      <div className=" d-flex flex-column align-items-center p-5" >
+         <Form className="p-5" >
+          <Form.Group  controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-           
+            <Form.Control type="email"  placeholder="Enter email" />
           </Form.Group>
 
-          <Form.Group controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+      
+
+          <Form.Group  controlId="formBasicPassword">
+            <Form.Label >Password</Form.Label>
+            <Form.Control  type="password" placeholder="Password" />
           </Form.Group>
-          <Form.Group controlId="formBasicCheckbox">
+          <Form.Group  controlId="formBasicCheckbox">
             <Form.Check type="checkbox" label="Remember me" />
           </Form.Group>
           <Button variant="primary" type="submit">
             Submit
           </Button>
-        </Form>
+
+
+          
+        </Form> 
       </div>
     );
   }
--- test/src/App.js
+++ test/src/App.js
@@ -1,77 +1,81 @@
-import React from "react";
+import { Component, Fragment } from 'react'
 
-class App extends React.Component {
-  constructor() {
-    super();
+class App extends Component {
 
-    this.state = {
-      userConnected: true,
-      userName: "Ali",
-    };
-  }
+    constructor() {
+        super()
 
-  funNavigation() {
-    return (
-      <nav>
-        <ul>
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/about">about</a>
-          </li>
-          <li>
-            <a href="/contact">contact</a>
-          </li>
-        </ul>
-      </nav>
-    );
-  }
+        this.state = {
+            username: "",
+            money: 35
+        }
 
-  aliContent(){
-    if (this.state.userConnected) {
-      return (
-        <section>
-          <h1>Konexio</h1>
-          <h2>Learning React</h2>
-        </section>
-      )
-    } else {
-      return (
-        <section>
-          <p>Vous devez vous connecter pour voir ce contenu</p>
-        </section>
-      )
+        this.updateUsername = this.updateUsername.bind(this)
+    }
 
+    updateUsername(event) {
+        console.log("updateUsername target", event.target);
+        console.log("updateUsername (si l'attribut existe dans le target. pas le cas pour un button par exemple)" ,event.target.type);
+        console.log("updateUsername value", event.target.value);
+
+        if (event.target.type === "text") {
+            this.setState({
+                username: event.target.value
+            })
+        } else if (event.target.id === "money") {
+            this.setState({
+                money: event.target.value
+            })
+        }
 
     }
-  }
 
-  renderUserName() {
-    return (
-      <h2>renderUserName. User Name: {this.state.userName}</h2>
-    )
+    // C'est pas le plus performant avec => fonctions mais voici une autre option
+    // updateUsernameArrow = (e) => {
+    //     // console.log("updateUsernameArrow", e.target );
 
+    //     console.log("value of input email is:", e.target.value);
+    // }
 
+    // C'est pas le plus performant avec => fonctions mais voici une autre option
+    updateUsernameArrow = (value) => {
+        console.log("value of input email is:", value);
 
-  }
-
-  render() {
-    let userName
-
-    if(this.state.userConnected) {
-      
+        // on pourrait faire quelque chose avec, par exemple mettre à jour un state (le state email n'existe pas encore, donc il faudrait le créer dans ce cas)
     }
-    return (
 
-      <div>
-        {this.funNavigation()}
-        {this.aliContent()}
-      </div>
-    )
+    render() {
+        return (
+            // <Fragment> // <> est un raccourci de Fragment, qu'on pourrais utiliser aussi comme React.Frament, si on avais importé React sans destructuration dans la première ligne
+            <>
+                <h1>Test Events:</h1>
 
-  }
+                <div>
+                    <label>Email</label>
 
+                    {/* <input type="email" onInput={this.updateUsernameArrow}></input> */} 
+
+                    {/* C'est pas le plus performant avec => fonctions mais voici une autre option */}
+                    <input type="email" onInput={(e) => this.updateUsernameArrow(e.target.value)}></input>
+                </div>
+
+                <div>
+                    <label>Username</label>
+                    <input id="username" type="text" onChange={this.updateUsername}></input>
+                </div>
+
+                <div>
+                    <input id="money" type="range" value={this.state.money} onChange={this.updateUsername}></input>
+
+                    <button onClick={this.updateUsername}>Click me!</button>
+                </div>
+
+                <p>My username is: {this.state.username}</p>
+                <p>My amount of money is: {this.state.money}</p>
+            </>
+            // </Fragment>
+        )
+    }
 }
 
-export default App;
+export default App
