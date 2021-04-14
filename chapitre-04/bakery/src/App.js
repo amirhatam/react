30f3bd74fd738540a8705d09a19a07f50063eff1
Amir <amirhatam136@gmail.com>
2021-04-14 16:47:28 +0200

    ""

--- chapitre-04/bakery/src/App.js
+++ chapitre-04/bakery/src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component, Fragment } from 'react'
+import Button from "./components/Button.jsx"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    constructor() {
+        super()
+
+        this.state = {
+          activeTab : "add ",
+          items : [] 
+        }
+
+        this.updateUsername = this.updateUsername.bind(this)
+    }
+
+    updateUsername(event) {
+        console.log("updateUsername target", event.target);
+        console.log("updateUsername (si l'attribut existe dans le target. pas le cas pour un button par exemple)" ,event.target.type);
+        console.log("updateUsername value", event.target.value);
+
+        if (event.target.type === "text") {
+            this.setState({
+                username: event.target.value
+            })
+        } else if (event.target.id === "money") {
+            this.setState({
+                money: event.target.value
+            })
+        }
+
+    }
+
+   
+    updateUsernameArrow = (value) => {
+        console.log("value of input email is:", value);
+
+    }
+
+    render() {
+        return (
+            <>
+              
+
+               
+              <h1>Bakery</h1>
+                <div>
+                    {/* <input id="money" type="range" value={this.state.money} onChange={this.updateUsername}></input> */}
+                  
+                    <button onClick={this.updateUsername}>Click me!</button>
+                    <button onClick={this.updateUsername}>Click me!</button>
+                    <button onClick={this.updateUsername}>Click me!</button>
+
+                </div>
+                    <input id="money" type="text" value={this.state.money} onChange={this.updateUsername}></input>
+
+                <p>My username is: {this.state.username}</p>
+                <p>My amount of money is: {this.state.money}</p>
+            </>
+            // </Fragment>
+        )
+    }
 }
 
-export default App;
+export default App
---
+++ chapitre-04/bakery/src/components/Button.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+ 
+class Button extends React.Component {
+	
+    isSelected  = () => {
+        // console.log(this);
+    }
+
+    onClick  = () => {
+        // console.log(this);
+    }
+
+
+    children = () => {
+        // console.log(this);
+    }
+
+
+
+  render() {
+    return (
+			<div >
+				
+			</div>
+		)
+  };
+}
+
+export default Button;
