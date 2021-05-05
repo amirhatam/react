46d502eec352a0f33b08e1c7e3d8653ce37f388d
Amir <amirhatam136@gmail.com>
2021-05-05 16:29:52 +0200

    initialise react app & ex 2

---
+++ chapitre-06/moovice/src/App.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { BrowserRouter, Route, Switch,Link } from "react-router-dom";
+
+import Home from "./view/Home"
+import Weekly from "./view/Weekly"
+import WeeklyBattle from "./view/WeeklyBattle"
+import Popular from "./view/Popular"
+import PopularBattle from "./view/PopularBattle"
+import Favorites from "./view/Favorites"
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+        <nav>
+          <ul>
+            <li><Link to="/"> Home</Link></li>
+            <li><Link to="/weekly"> Weekly</Link></li>
+            <li><Link to="/weekly-battle"> WeeklyBattle</Link></li>
+            <li><Link to="/popular"> Popular</Link></li>
+            <li><Link to="/popular-battle"> PopularBattle</Link></li>
+            <li><Link to="/favorites"> Favorites</Link></li>
+          </ul>
+        </nav>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/weekly" component={Weekly} />
+            <Route path="/weekly-battle" component={WeeklyBattle} />
+            <Route path="/popular" component={Popular} />
+            <Route path="/popular-battle" component={PopularBattle} />
+            <Route path="/favorites" component={Favorites} />
+          </Switch>
+          
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ chapitre-06/moovice/src/view/Weekly.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Weekly extends Component {
+    render() {
+        console.log("weekly test:");
+        return (
+            <div>
+                 <h1>Weekly</h1>
+
+            </div>
+        )
+    }
+}
